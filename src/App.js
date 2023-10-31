5d733622773dcd25573d5735293bd1c467c72c73
aquino-t <aquino.t@tutanota.com>
2023-10-31 14:44:45 -0300

    :feat:  botão add e close, finalização

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -28,12 +29,17 @@ function App() {
   const toggleReminder = (id) => {
     setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
   }
+
+ 
   
   
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTaks}/>
+      <Header 
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTaks}/>}
       {tasks.length > 0 ? <Tasks 
         tasks={tasks} 
         onDelete={deleteTask}
