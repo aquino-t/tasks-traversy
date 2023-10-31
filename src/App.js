50749a391a2ba8e2b06c4d2d2443e04e0418d881
aquino-t <aquino.t@tutanota.com>
2023-10-31 14:17:25 -0300

    :feat|: criação do componente AddTask

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react"
 import Header from './components/Header';
 import './App.css';
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -12,6 +13,13 @@ function App() {
       reminder: true,
     }
   ])
+
+  const addTaks = (task) => {
+    const id = Math.floor(Math.random() *
+    10000) + 1
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
 
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) =>task.id !== id))
@@ -25,6 +33,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTaks}/>
       {tasks.length > 0 ? <Tasks 
         tasks={tasks} 
         onDelete={deleteTask}
