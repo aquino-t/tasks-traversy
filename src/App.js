e7b23255015e710b1a40fd3dd376d7109d796dbe
aquino-t <aquino.t@tutanota.com>
2023-10-31 13:42:28 -0300

    :feat: implemetação delete function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { useState } from "react"
+import Header from './components/Header';
 import './App.css';
+import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Entrevista de Emprego",
+      day: "Amanhã",
+      reminder: true,
+    }
+  ])
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) =>task.id !== id))
+  }
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      {tasks.length > 0 ? <Tasks 
+        tasks={tasks} 
+        onDelete={deleteTask} 
+      /> : "No tasks to show"}
     </div>
   );
 }
